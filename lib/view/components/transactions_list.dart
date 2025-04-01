f0ed5dbafd6601d560cd32a73b3d48bf1450f238
BlackFrisian13 <sonaivanyan13@gmail.com>
2025-04-01 17:15:44 +0200

    Transactions list added

--- lib/view/components/transactions_list.dart
+++ lib/view/components/transactions_list.dart
@@ -12,6 +12,22 @@ class TransactionsList extends StatelessWidget {
         decoration: const BoxDecoration(
             color: Colors.white
         ),
+        child: ListView(
+          children: [
+            ListTile(
+              leading: Icon(Icons.attach_money, color: Colors.teal),
+              title: Text('Income'),
+              subtitle: Text('You got \$ 1,000.00'),
+              trailing: Text('\$ 1,000.00', style: TextStyle(fontSize: 14)),
+            ),
+            ListTile(
+              leading: Icon(Icons.money_off, color: Colors.red),
+              title: Text('Expense'),
+              subtitle: Text('You spent \$ 500.00'),
+              trailing: Text('- \$ 500.00', style: TextStyle(fontSize: 14)),
+            )
+          ],
+        ),
       ),
     );
   }
